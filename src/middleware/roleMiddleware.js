2169ce8b2c84ec74dc1dafe7dafa7b8f3094d599
Shubham Jalwaniya <jalwaniyash702@gmail.com>
2025-10-16 00:16:27 +0530

    Access denied page converted into html

--- src/middleware/roleMiddleware.js
+++ src/middleware/roleMiddleware.js
@@ -1,7 +1,9 @@
+import path from 'path';
+
 const authorizerole = (...allowedRoles) => {
   return (req, res, next) => {
     if (!allowedRoles.includes(req.user.role)) {
-      return res.status(403).render("accessdenied");
+      return res.status(403).sendFile(path.join(process.cwd(), 'src', 'public', 'accessdenied.html'));
     }
     next();
   };
